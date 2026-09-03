40da32d70dcd1ddc1c1b53dea6213be0ef2c2155
Vanesha Hari <pid@vt.edu>
2026-09-02 21:51:01 -0400

    This is my Message.cpp code

--- src/Message.cpp
+++ src/Message.cpp
@@ -1,42 +1,40 @@
 #include "Message.hpp"
 
 Message::Message(MessageRole role, std::string text) {
-    // TODO: implement according to the M1 specification.
-
+    role_ = role;
+    text_ = text;
 }
 
 bool Message::operator==(const Message& other) const {
-    // TODO
-
+    return role_ == other.role_ && text_ == other.text_;
 }
 
 bool Message::operator!=(const Message& other) const {
-    // TODO
+    return !(*this == other);
 
 }
 
 MessageRole Message::role() const noexcept {
-    // TODO
-    
-    return MessageRole::User;
+    return role_;
 }
 
 const std::string& Message::text() const noexcept {
-    // TODO
+    return text_;
     
 }
 
 void Message::setRole(MessageRole role) noexcept {
-    // TODO
+    role_ = role;
     
 }
 
 void Message::setText(std::string text) {
-    // TODO
+    text_ = text;
     
 }
 
 bool Message::empty() const noexcept {
-    // TODO
+    return text_.empty();
+
     
 }
